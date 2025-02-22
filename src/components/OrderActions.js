216496329ff1c5cbbcf7e984266f50f2f257ad1f
jannie <jannien@utexas.edu>
2025-02-21 18:16:32 -0600

    fixed alert

--- src/components/OrderActions.js
+++ src/components/OrderActions.js
@@ -1,16 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const OrderActions = ({ subtotal, setTotal, itemCounts, setItemCounts }) => {
-    const [orderClicked, setOrderClicked] = useState(false);
-
     const clearOrder =  () => {
       setTotal(0);
       setItemCounts(new Map());
     };
 
     const handleOrderClick = () => {
-      setOrderClicked(true);
-
       if (itemCounts.size > 0) {
         let orderSummary = '';
         itemCounts.forEach((count, item) => {
@@ -19,6 +15,8 @@ const OrderActions = ({ subtotal, setTotal, itemCounts, setItemCounts }) => {
 
         // Construct and show the alert
         alert(`Your order has been placed!\n\nOrder:\n${orderSummary}\nTotal: $${subtotal.toFixed(2)}`);
+      } else {
+        alert(`No items in cart. Please add items before placing an order.`);
       }
     };
 
@@ -31,11 +29,6 @@ const OrderActions = ({ subtotal, setTotal, itemCounts, setItemCounts }) => {
           <button class="orderActionButton" onClick={handleOrderClick}>Order</button>
           <button class="orderActionButton" onClick={clearOrder}>Clear all</button>
         </div>
-        {orderClicked && itemCounts.size === 0 && (
-          <div className="emptyCartMessage">
-            <p>No items in cart. Please add items before placing an order.</p>
-          </div>
-        )}
       </div>
     );
 };
